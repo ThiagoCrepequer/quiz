527ac8f25f15867ef8d73ca654fa8cf902bb0766
Thiago de Souza Crepequer <thiago.crepequer@hotmail.com>
2022-12-21 22:56:07 -0300

    update

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const getPergunta = require('./mongodb')
+
+var contagem
 
 // API para o metodo get
 app.get('/pergunta', (req, res) => {
-    var test =  [{ pergunta: 'Qual a capital do Brasil', alternativas: { 1: 'São Paulo', 2: 'Rio de Janeiro', 3: 'Fortaleza', 4: 'Brasília' }}]
-    
-    res.send(test)
+    getPergunta(contagem).then(pergunta => {
+        var pergunta_alternativas = {
+            alternativas: pergunta.alternativas,
+            pergunta: pergunta.pergunta
+        }
+        res.send(pergunta_alternativas)
+    });
 });
 
 // Metodo post
 app.use(bodyParser.json());
 
 app.post('/reposta', (req, res) => {
-    console.log(req.body);
+    var alternativa = req.body
+    getPergunta(contagem).then(pergunta => {
+        console.log(pergunta.correta == alternativa.alternativa)
+        if(pergunta.correta == alternativa.alternativa) {
+            var alternativa_correta = {
+                resultado: "correto"
+            }
+
+            contagem++
+
+            res.json(alternativa_correta)
+        } else {
+            var alternativa_errada = {
+                resultado: "errado"
+            }
+            res.json(alternativa_errada)
+        }
+    });
 });
 
 // Pagina principal
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
     app.use(express.static('src'))
+
+    contagem = 1
 });
 
 app.listen(80, () => {
--- src/scripts/inicio.js
+++ src/scripts/inicio.js
@@ -2,6 +2,6 @@ var controla_comeco = 0
 
 document.addEventListener('keypress', function(res) {
     if(controla_comeco == 0 && res.key == 'Enter') {
-        var alternativas = get_pergunta()
+        get_pergunta()
     }
 })
