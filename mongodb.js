bd78d7cf211e89a3f267a426600ee85bfc314a43
Thiago de Souza Crepequer <thiago.crepequer@hotmail.com>
2022-12-23 18:18:52 -0300

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -4,6 +4,7 @@ dotenv.config({debug: true})
 
 const dbname = 'perguntas';
 const url = process.env.URL_MONGO;
+console.log(url)
 
 var client
 
@@ -19,6 +20,7 @@ connectClient().then(res => {
 
 async function getPergunta(valor) {
   try {
+    console.log('Houve uma tentativa de buscar uma questão no banco de dados')
     const perguntasCollection = client.db(dbname).collection('perguntas');
     
     const pergunta = await perguntasCollection.findOne({ sequencia: `${valor}` });
